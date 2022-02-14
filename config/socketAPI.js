4ab0e213b41690c5c9c0efb78ade3e043dcef52b
DanK <ddad9583@gmail.com>
2022-02-14 19:32:30 +0900

    [66] 소켓채팅 구현.

--- config/socketAPI.js
+++ config/socketAPI.js
@@ -19,26 +19,34 @@ toiletChatList.use(verifyPoodadakTokenSocket);
 toiletChatList.on("connection", async (socket) => {
   const toiletId = socket.nsp.name.split("-")[1];
   const userId = socket.userId;
+  const roomName = socket.handshake.query.room;
+  const roomDBId = socket.handshake.query.roomDBId;
   let chatroomId;
 
-  socket.join(userId);
+  socket.join(roomName);
 
   // eslint-disable-next-line no-console
   console.log(
-    `User ${socket.id} ❗️connected to namespace 🚽toiletId-${toiletId} and joined 🚪room ${userId}`
+    `User ${socket.id} ❗️connected to namespace 🚽toiletId-${toiletId} and joined 🚪room ${roomName}`
   );
 
   try {
-    const createdChatroom = await Chatroom.create({
-      owner: userId,
-      toilet: toiletId,
-      isLive: true,
-    });
+    const connectedChatroom = await Chatroom.findById(roomDBId);
+    if (connectedChatroom) {
+      chatroomId = connectedChatroom._id;
+    } else {
+      const createdChatroom = await Chatroom.create({
+        owner: userId,
+        toilet: toiletId,
+        isLive: true,
+      });
+
+      chatroomId = createdChatroom._id;
+    }
 
     await Toilet.findByIdAndUpdate(toiletId, { isSOS: true });
 
-    chatroomId = createdChatroom._id;
-    socket.emit("createChatroom", chatroomId);
+    socket.emit("joinChatroom", chatroomId);
   } catch (error) {
     socket.emit(
       "db-error",
@@ -56,24 +64,56 @@ toiletChatList.on("connection", async (socket) => {
   }, THIRTY_MINUTES);
 
   socket.on("loadChatList", async (chatroomId) => {
-    const { chatList } = await Chatroom.findById(chatroomId, "chatList");
-    // console.log("!!!cl", chatList);
-    socket.emit("findChatList", chatList);
+    try {
+      const { chatList } = await Chatroom.findById(chatroomId, "chatList");
+      socket.emit("findExistingChatList", chatList);
+    } catch (error) {
+      socket.emit(
+        "db-error",
+        new ErrorWithStatus(
+          error,
+          500,
+          ERROR_MESSAGES.FAILED_TO_COMMUNICATE_WITH_DB
+        ).toPlainSocketErrorObject()
+      );
+      socket.disconnect(true);
+    }
   });
 
-  socket.on("sendChat", async (payload) => {
+  socket.on("sendChat", async (chat) => {
+    const isChatEmpty = !chat.message.trim().length;
+
+    if (isChatEmpty) {
+      return;
+    }
+
     clearTimeout(timerId);
 
     timerId = setTimeout(async () => {
+      socket.emit("chatTimeout");
       socket.disconnect(true);
     }, THIRTY_MINUTES);
 
-    // console.log("rc", payload);
-    await Chatroom.findByIdAndUpdate(chatroomId, {
-      $push: { chatList: payload },
-    });
+    // console.log("rc", chat);
+    try {
+      const existingChatroom = await Chatroom.findByIdAndUpdate(chatroomId, {
+        $push: { chatList: chat },
+      });
 
-    socket.broadcast.to(userId).emit("receiveChat", payload);
+      chat.updatedChatListLength = existingChatroom.chatList.length + 1;
+
+      socket.broadcast.to(roomName).emit("receiveChat", chat);
+    } catch (error) {
+      socket.emit(
+        "db-error",
+        new ErrorWithStatus(
+          error,
+          500,
+          ERROR_MESSAGES.FAILED_TO_COMMUNICATE_WITH_DB
+        ).toPlainSocketErrorObject()
+      );
+      socket.disconnect(true);
+    }
   });
 
   socket.on("disconnect", async () => {
@@ -88,6 +128,8 @@ toiletChatList.on("connection", async (socket) => {
         }
       }
       await updateSOS(toiletId);
+
+      socket.broadcast.to(roomName).emit("leaveChat");
     } catch (error) {
       // eslint-disable-next-line no-console
       console.log(ERROR_MESSAGES.FAILED_TO_COMMUNICATE_WITH_DB);
