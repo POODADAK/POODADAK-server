3d40b87738e614ea4320a21bb3aee843ba84a217
DanK <ddad9583@gmail.com>
2022-02-18 19:32:20 +0900

    [hotfix] remove unnecessary route.

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,14 +4,12 @@ const router = express.Router();
 const {
   signinKakao,
   signinNaver,
-  eraseCookie,
   sendVerified,
 } = require("../controller/authController");
 const verifyPoodadakToken = require("../middlewares/verifyPoodadakToken");
 
 router.post("/kakao", signinKakao);
 router.post("/naver", signinNaver);
-router.post("/token-elimination", eraseCookie);
 router.post("/token-verification", verifyPoodadakToken, sendVerified);
 
 module.exports = router;
