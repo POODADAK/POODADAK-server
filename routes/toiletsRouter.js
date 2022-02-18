86122ee4dc70e4877087df125adacf18ffec9125
Jinho <breezjin@gmail.com>
2022-02-18 16:34:44 +0900

    [88] toilet api update (#29)

--- routes/toiletsRouter.js
+++ routes/toiletsRouter.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const {
   getNearToiletsList,
+  getMapToiletsList,
   getReviewsList,
   getToilet,
 } = require("../controller/toiletsController");
 
 router.get("/", getNearToiletsList);
+router.get("/mapToiletsList", getMapToiletsList);
 router.get("/:toiletId", getToilet);
 router.get("/review/:toiletId", getReviewsList);
 
